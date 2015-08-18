b0e81764a02df2f3a7fd199025f5cef81939cd83
sorrycc <sorrycc@gmail.com>
2015-08-18 16:19:29 +0800

    feat: 配置自定义支持函数

--- examples/customize-with-reactcss/webpack.config.merge.js
+++ examples/customize-with-reactcss/webpack.config.merge.js
@@ -1,15 +1,10 @@
 'use strict';
 
-module.exports = {
-
-  module: {
-
-    loaders: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel!react-map-styles'
-      }
-    ]
-  }
+module.exports = function(config) {
+  config.module.loaders = [{
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    loader: 'babel!react-map-styles'
+  }];
+  return config;
 };
