b724347d238035aeac4d750bf3881f8dc1148593
sorrycc <sorrycc@gmail.com>
2015-10-27 17:34:57 +0800

    refactor(init): antd-init as external pkg

--- boilerplate/src/entry/index.jsx
+++
@@ -1,6 +0,0 @@
-import '../common/lib';
-import App from '../component/App';
-import React from 'react';
-import ReactDom from 'react-dom';
-
-ReactDom.render(<App />, document.getElementById('react-content'));
